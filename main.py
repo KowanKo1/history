a23f356604da01b9dfe896a83649f67cb8ef0703
Valencius Apriady <v.apriady@gmail.com>
2024-12-08 11:15:52 +0700

    feat: sync model for all service

--- main.py
+++ main.py
@@ -6,23 +6,17 @@ from dotenv import load_dotenv
 from database import init_db, get_db  # Import your database utilities
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from models import Transaction
 
 # Load environment variables
 load_dotenv()
-INVENTORY_URL = os.getenv("INVENTORY_URL")
+INVENTORY_SERVICE_URL = os.getenv("INVENTORY_SERVICE_URL")
+AUTHENTICATION_SERVICE_URL = os.getenv("AUTHENTICATION_SERVICE_URL")
 
 # Initialize database
 init_db()
 
 app = FastAPI()
-
-# Transaction model for the database
-class Transaction(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    item_id: int
-    quantity: int
-    transaction_type: str
-    timestamp: datetime
 
 @app.post("/api/transactions", response_model=Transaction)
 async def create_transaction(
@@ -33,10 +27,17 @@ async def create_transaction(
     """
     Endpoint to create a transaction. Only accessible from INVENTORY_URL.
     """
-    print(request.headers)
-    print(request.headers.get('origin'))
-    if request.headers.get('origin') != INVENTORY_URL:
+    # print(request.headers)
+    # print(request.headers.get('origin'))
+    # print(INVENTORY_URL)
+    if request.headers.get('origin') != INVENTORY_SERVICE_URL:
         raise HTTPException(status_code=403, detail="Origin not allowed.")
+    
+    print('transaction received: ', transaction)
+    
+    transaction.createdAt = datetime.now()
+    transaction.modifiedAt = datetime.now()
+    transaction.timestamp = datetime.now()
 
     # Add the transaction to the database
     db.add(transaction)
@@ -45,8 +46,10 @@ async def create_transaction(
     return transaction
 
 @app.get("/api/transactions", response_model=List[Transaction])
-def get_transactions(db: Session = Depends(get_db)):
+def get_transactions(request: Request, db: Session = Depends(get_db)):
     """
     Endpoint to retrieve all transactions.
     """
+    if request.headers.get('origin') != AUTHENTICATION_SERVICE_URL:
+        raise HTTPException(status_code=403, detail="Origin not allowed.")
     return db.exec(select(Transaction)).all()
